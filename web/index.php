af203367b817eba73d9b07c664a4c8c61fc0b814
Patrick Emond <emondpd@mcmaster.ca>
2024-10-03 16:26:09 -0400

    Feature: disabling the web UI

--- web/index.php
+++ web/index.php
@@ -1,20 +1,6 @@
 <?php
 /**
- * Main web script which drives the application
+ * Dogwood does not have a web UI
  */
 
-if( !array_key_exists( 'REDIRECT_URL', $_SERVER ) ||
-    0 == preg_match( '#/app/[^/]+/[^/]+.extend(\.min)?.js#', $_SERVER['REDIRECT_URL'] ) )
-{
-  // load web-script common code
-  require_once '../settings.ini.php';
-  require_once '../settings.local.ini.php';
-  require_once $SETTINGS['path']['CENOZO'].'/src/bootstrap.class.php';
-  $bootstrap = new \cenozo\bootstrap();
-  $bootstrap->initialize( 'ui' );
-}
-else if( 0 != preg_match( '#\.js$#', $_SERVER['REDIRECT_URL'] ) )
-{
-  // make sure that javascript files have the correct header (not html)
-  header( 'Content-Type: application/javascript' );
-}
+header( 'HTTP/1.1 404' );
