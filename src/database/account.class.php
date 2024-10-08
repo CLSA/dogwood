62355d6aaac32cc44134a67f184e0b17f57195c3
Patrick Emond <emondpd@mcmaster.ca>
2024-10-08 16:52:14 -0400

    Bug: fixing double-hash bug

--- src/database/account.class.php
+++ src/database/account.class.php
@@ -12,14 +12,4 @@ use cenozo\lib, cenozo\log, dogwood\util;
 /**
  * account: record
  */
-class account extends \cenozo\database\record
-{
-  /** 
-   * Override parent method
-   */
-  public function __set( $column_name, $value )
-  {
-    // hash the password
-    parent::__set( $column_name, $column_name == 'password' ? password_hash( $value, PASSWORD_BCRYPT ) : $value );
-  }
-}
+class account extends \cenozo\database\record {}
