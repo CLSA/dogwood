cb137166b7998982a228d4aa361591773a56231d
Patrick Emond <emondpd@mcmaster.ca>
2026-06-11 16:04:45 -0400

    Feature: creating aux/read_schema script to update db files

--- db/table/account.sql
+++ db/table/account.sql
@@ -1,20 +1,19 @@
 CREATE TABLE account (
-  id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  update_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-  create_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  organization_id INT(10) UNSIGNED NOT NULL,
-  username VARCHAR(45) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  password_type ENUM('whirlpool', 'bcrypt') NULL DEFAULT NULL,
-  email VARCHAR(255) NULL DEFAULT NULL,
+  id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  update_timestamp timestamp NOT NULL DEFAULT current_timestamp()
+    ON UPDATE current_timestamp(),
+  create_timestamp timestamp NOT NULL DEFAULT current_timestamp(),
+  organization_id int(10) unsigned NOT NULL,
+  username varchar(45) NOT NULL,
+  password varchar(255) NOT NULL,
+  password_type enum('whirlpool','bcrypt') DEFAULT NULL,
+  email varchar(255) DEFAULT NULL,
   PRIMARY KEY (id),
-  INDEX fk_organization_id (organization_id ASC),
-  UNIQUE INDEX uq_organization_id_name (organization_id ASC, username ASC),
+  UNIQUE KEY uq_organization_id_name (organization_id,username),
+  KEY fk_organization_id (organization_id),
   CONSTRAINT fk_user_organization_id
     FOREIGN KEY (organization_id)
-    REFERENCES dogwood.organization (id)
+    REFERENCES organization (id)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_general_ci;
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- db/table/user_has_organization.sql
+++ db/table/user_has_organization.sql
@@ -1,21 +1,20 @@
 CREATE TABLE user_has_organization (
-  user_id INT(10) UNSIGNED NOT NULL,
-  organization_id INT(10) UNSIGNED NOT NULL,
-  update_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-  create_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY (user_id, organization_id),
-  INDEX fk_organization_id (organization_id ASC),
-  INDEX fk_user_id (user_id ASC),
+  user_id int(10) unsigned NOT NULL,
+  organization_id int(10) unsigned NOT NULL,
+  update_timestamp timestamp NOT NULL DEFAULT current_timestamp()
+    ON UPDATE current_timestamp(),
+  create_timestamp timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (user_id,organization_id),
+  KEY fk_organization_id (organization_id),
+  KEY fk_user_id (user_id),
+  CONSTRAINT fk_user_has_organization_organization_id
+    FOREIGN KEY (organization_id)
+    REFERENCES organization (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT fk_user_has_organization_user_id
     FOREIGN KEY (user_id)
     REFERENCES cenozo.user (id)
     ON DELETE CASCADE
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_user_has_organization_organization_id
-    FOREIGN KEY (organization_id)
-    REFERENCES dogwood.organization (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_general_ci;
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
